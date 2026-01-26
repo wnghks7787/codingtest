e6a78e6044b06aab67709128dba55ae6e36726a2
wnghks7787 <wnghks7787@naver.com>
2026-01-26 15:44:50 +0900

    update

---
+++ baek1759/baek1759.py
@@ -0,0 +1,11 @@
+VOWELS = ['a', 'e', 'i', 'o', 'u']
+
+L, C = map(int, input().split())
+alphabets = list(map(str, input().split()))
+
+def main():
+    # 쉬운것부터 하고 오자. 뇌를 말랑말랑하게 하고...
+
+
+if __name__ == '__main__':
+    main()
