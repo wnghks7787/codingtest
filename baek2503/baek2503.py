954d04d62fb728c0a60f214db2541c3e8d6279fe
wnghks7787 <wnghks7787@naver.com>
2026-02-11 15:44:33 +0900

    update baek2503 with python

--- baek2503/baek2503.py
+++ baek2503/baek2503.py
@@ -1,49 +1,46 @@
-def comp(i, j, k, lsts):
-    correct = 0
+from itertools import permutations
 
-    for lst in lsts:
+possibleNums = permutations(range(1, 10), 3)
+
+predicts = []
+count = 0
+N = int(input())
+for _ in range(N):
+    predicts.append(input().split())
+
+for possibleNum in possibleNums:
+    check = True
+
+    for predict in predicts:
         strike = 0
         ball = 0
 
-        x = int(lst[0][0])
-        y = int(lst[0][1])
-        z = int(lst[0][2])
+        # 체크 로직
+        a = int(predict[0][0])
+        b = int(predict[0][1])
+        c = int(predict[0][2])
 
-        if x == i:
+        x = possibleNum[0]
+        y = possibleNum[1]
+        z = possibleNum[2]
+
+        if a == x:
             strike += 1
-        elif x == j or x == k:
+        elif a == y or a == z:
             ball += 1
-        if y == j:
+        if b == y:
             strike += 1
-        elif y == i or y == k:
+        elif b == x or b == z:
             ball += 1
-        if z == k:
+        if c == z:
             strike += 1
-        elif z == i or z == j:
+        elif c == x or c == y:
             ball += 1
+        
+        if strike != int(predict[1]) or ball != int(predict[2]):
+            check = False
 
-        if strike == int(lst[1]) and ball == int(lst[2]):
-            correct += 1
+    if check:
+        count += 1
 
-    if correct == len(lsts):
-        return 1
-    else:
-        return 0
-
-N = input()
-
-predicts = []
-
-for _ in range(0, int(N)):
-    predict = input().split()
-    predicts.append(predict)
-
-num = 0
-
-for i in range(1, 10):
-    for j in range(1, 10):
-        for k in range(1, 10):
-            if i != j and i != k and j != k:
-                num += comp(i, j, k, predicts)
-
-print(num)
+print(count)
