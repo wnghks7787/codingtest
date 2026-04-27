0a147708e3a56afab6e58241a3ed58737196a843
wnghks7787 <wnghks7787@naver.com>
2026-04-27 22:12:21 +0900

    solved codetree 1-1-1-9 with c++

---
+++ C++/codetree/Trail1/Ch01/1. 기본 출력/9. 다양하게 출력/answer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << "Total days in Year" << endl;
+    cout << 365 << endl;
+    cout << "Circumference rate" << endl;
+    cout << "3.1415926535" << endl;
+    return 0;
+}
